7d3dc3dc107c76d001c4008977409ef5798389a1
Jose <jocvegar@gmail.com>
2020-06-25 15:10:45 -0600

    Adds outside js and changeges icons

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import "./registerServiceWorker";
 import router from "./router";
 import VueFirestore from "vue-firestore";
 import "./assets/css/app.css";
+import LoadScript from "vue-plugin-load-script";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueFirestore);
+Vue.use(LoadScript);
+Vue.loadScript("./js/jquery.min.js");
+Vue.loadScript("./js/jquery.scrollex.min.js");
+Vue.loadScript("./js/jquery.scrolly.min.js");
+Vue.loadScript("./js/browser.min.js");
+Vue.loadScript("./js/breakpoints.min.js");
+Vue.loadScript("./js/util.js");
+Vue.loadScript("./js/main.js");
 
 new Vue({
   router,
